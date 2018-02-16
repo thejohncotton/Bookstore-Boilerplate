6f77868af820e19c8471c50d9512c943221cb1b5
Rakesha McIntyre <robinson.rakesha@gmail.com>
2018-02-16 00:04:00 -0500

    Nav Bar

--- js/bookstore.js
+++ js/bookstore.js
@@ -3,7 +3,7 @@ var book1 = {
     "name": "Lasagna: A Retrospective",
     "author": "Garfield",
     "pictureUrl": "http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
-    "price": 24,
+    "price": 24.00,
     "sellingPoints": [
         "Lasagna is delicious.",
         "The essential guide to Italian casseroles of all types.",
