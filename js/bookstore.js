366b0b0d693fc04e6fbeba4316f7b0e83be41b64
Keri Hedman <kk37_2010@yahoo.com>
2018-02-27 20:59:46 -0500

    Clean reset and added projInfo with music and audioBooks

--- js/bookstore.js
+++ js/bookstore.js
@@ -1,74 +1,70 @@
-var bookInfo = [ // Array of books
-  {
-    "id": 0,
-    "title": "",
-    "author": "",
-    "price": 0,
-    "imageUrl": "",
-    "sellingPoints": [
-        "","",""
-    ]
-  } // basic object information for a single book
-];
+var prodInfo = {
+  "books":  [ { // Array of books with basic object information
+              "id": 0,
+              "title": "",
+              "author": "",
+              "price": 0,
+              "imageUrl": "",
+              "sellingPoints": ["","",""]
+            } ],
+  "music":  [ { // Array of books with basic object information
+              "id": 0,
+              "title": "",
+              "author": "",
+              "price": 0,
+              "imageUrl": "",
+              "sellingPoints": ["","",""]
+            } ],
+  "audioBooks": [ { // Array of audio books with basic object information
+                  "id": 0,
+                  "title": "",
+                  "author": "",
+                  "price": 0,
+                  "imageUrl": "",
+                  "sellingPoints": ["","",""]
+                } ],
+} // Object containing three arrays: books, audio books and music
 
-var appendToPage = (book) => {
-//     $('#book-id li p').text(book.id);
-//     $('#book-name li p').text(book.title);
-//     $('#book-author li p').text(book.author);
-//     $('#book-price li p').text(book.price);
-//     $('#book-picture li img').attr("src", book.imageUrl);
-// console.log(book.imageUrl);
-//     // loop through array to get selling points.
-//     var sellingPtsIndex = 0;
-//     while (sellingPtsIndex < book.sellingPoints.length) {
-//       $('#book-sellingPoints ul').append('<li>' +
-//         book.sellingPoints[sellingPtsIndex] + '</li>');
-//       sellingPtsIndex ++;
-//     };
-// console.log(book); // debug tool
 
-// Copy of HTML code layout for a single book
-// <div class="book" id="book1">
-//   <h3>Book 1</h3>
-//   <ul id="bookwrapper">
-//     <div id="book-id"><li>id: <p></p></li></div>
-//     <div id="book-name"><li>name: <p></p></li></div>
-//     <div id="book-author"><li>author: <p></p></li></div>
-//     <div id="book-price"><li>price: <p></p></li></div>
-//     <div id="book-picture"><li><img src="http://via.placeholder.com/125x185" alt="Book Image"></li></div>
-//   </ul>
-//     <p>Selling Points:</p>
-//     <div id="book-sellingPoints">
-//       <ul>
-//
-//       </ul>
-//     </div>
-// </div>
-    $("#bookList").append('<a href="#book' + book.id + '">'); // Hyper link jump to a book
-    $("#bookList").append('<div class="book" id="book' + book.id + '"></a>');
-    $("#bookList").append('<h3>Book ' + book.id + ':</h3>');
-    $("#bookList").append('<ul class="bookwrapper">');
-    $("#bookList").append('<div class="book-id"><li>ID: ' +
-                         book.id + '</li></div>');
-    $("#bookList").append('<div class="book-name"><li>Title: ' +
-                           book.title + '</li></div>');
-    $("#bookList").append('<div class="book-author"><li>Author: ' +
-                           book.author + '</li></div>');
-    $("#bookList").append('<div class="book-price"><li>Price: ' +
-                           book.price + '</li></div>');
-    $("#bookList").append('<div class="book-picture"><li><img src="' +
-                           book.imageUrl + '" alt="Book Image"></li></div>');
-    $("#bookList").append('</ul><p>Selling Points: </p>');
-    $("#bookList").append('<div class="book-sellingpoints"><ul>');
-      // loop through array to get selling points.
-     var sellingPtsIndex = 0;
-     while (sellingPtsIndex < book.sellingPoints.length) {
-       $("#bookList").append('<li>' +
-         book.sellingPoints[sellingPtsIndex] + '</li>');
-       sellingPtsIndex ++;
-     };
-     $("#bookList").append('</ul></div></div>');
-     $("#bookList").append('<br>')
+var appendToPage = (prod, type) => {
+  switch (type) { // Creating single object for code below based on type
+    case music:       let obj = prod.music; break;
+    case books:       let obj = prod.books; break;
+    case audioBooks:  let obj = prod.audioBooks; break;
+    default:          console.log('Error in type selection for appendToPage.')
+  }
+
+  // For the HTML, book is the original product.
+  $("#bookList").append('<a href="#book' + obj.id + '">'); // Hyper link jump to a book
+  $("#bookList").append('<div class="book" id="book' + obj.id + '"></a>');
+  $("#bookList").append('<h3>Book ' + obj.id + ':</h3>');
+  $("#bookList").append('<ul class="bookwrapper">');
+  $("#bookList").append('<div class="book-id"><li>ID: ' +
+                        obj.id + '</li></div>');
+  $("#bookList").append('<div class="book-name"><li>Title: ' +
+                        obj.title + '</li></div>');
+  $("#bookList").append('<div class="book-author"><li>Author: ' +
+                        obj.author + '</li></div>');
+  $("#bookList").append('<div class="book-price"><li>Price: ' +
+                        obj.price + '</li></div>');
+  $("#bookList").append('<div class="book-picture"><li><img src="' +
+                        obj.imageUrl + '" alt="Book Image"></li></div>');
+  $("#bookList").append('</ul><p>Selling Points: </p>');
+  $("#bookList").append('<div class="book-sellingpoints">');
+  $("#bookList").append('<ul>');
+  // Loop through array to get selling points.
+  obj.sellingpoints.forEach ( function(sellingPoint) {
+    $("#bookList").append('<li>' + sellingPoint + '</li>');
+  } );
+  $("#bookList").append('</ul></div></div>');
+  $("#bookList").append('<br>');
+}
+
+var appendToJSON = () => {
+  // Append to JSON file the current bookInfo object
+  var bookJSON = JSON.stringify(bookInfo);
+
+  // localStorage.setItem ( 'bookstore.json', bookJSON );
 }
 
 $( "form" ).on( "submit", ( event ) => {
@@ -77,18 +73,20 @@ $( "form" ).on( "submit", ( event ) => {
 
     event.preventDefault();
 
+    let type =
+
     formObject.id = bookInfo.length;
     formObject.sellingPoints = [];
 
     data.forEach( ( field ) => {
         if( field.name === "sP-1" || field.name === "sP-2" || field.name === "sP-3"){
             formObject.sellingPoints.push( field.value )
-        }
-        else {
+        } else {
             formObject[ field.name ] = field.value;
         }
     } );
-    bookInfo.push( formObject ); // Adding book to object array
-    appendToPage( formObject ); // Adding book info to the webpage
+    prodInfo.push( formObject ); // Adding book to object array
+    appendToPage( formObject, type ); // Adding book info to the webpage
+    appendToJSON( ); // Adding book to JSON file
     $('form')[0].reset(); // Resetting form
 });
